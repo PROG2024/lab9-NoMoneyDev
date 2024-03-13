8c3b7312df862b35a6cc1c0c38b9adb5a8ab0f2d
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2024-03-13 09:32:13 +0700

    Added test case specified

--- test_circle.py
+++ test_circle.py
@@ -8,5 +8,28 @@ Write unit tests as described in README.md.
 
 """
 from circle import Circle
+import unittest
+import math
 
-# TODO write 3 tests as described above
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.circle = Circle(7)
+
+    def test_add_area_normal(self):
+        new_circle = Circle(7)
+        added_circle = self.circle.add_area(new_circle)
+        self.assertEqual(math.sqrt(98), added_circle.get_radius())
+        self.assertEqual(math.pi*98, added_circle.get_area())
+
+    def test_add_0_radius_circle(self):
+        new_circle = Circle(0)
+        added_circle = self.circle.add_area(new_circle)
+        self.assertEqual(self.circle.get_radius(), added_circle.get_radius())
+        self.assertEqual(self.circle.get_area(), added_circle.get_area())
+
+    def negative_circle(self):
+        with self.assertRaises(ValueError):
+            new_circle = Circle(-1)
+
+
